cb6287c11c1775df8fe897151fc546bda7474001
Daniel Garcia Molero <dangar13@icloud.com>
2017-12-13 02:34:17 +0000

    Started experiments with PHP and database conection

---
+++ coursework/connect_db.php
@@ -0,0 +1,15 @@
+ <?php
+$serverAddr = "91.121.109.58"; //Is port needed? (Redirect port to match the MySQL port)
+$username = "adsuser";
+$password = "1234512345";
+$database = "ADSStudents";
+
+// Create connection
+$conn = new mysqli($serverAddr, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+// echo "Connected successfully";
+?>
---
+++ coursework/index.php
@@ -0,0 +1,68 @@
+<html>
+<head>
+    <meta charset=UTF-8>
+	<title>Student groups</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+	<h1>Student Groups for 6COM9051: More Web Scripting</h1>
+
+	<label for "selectedGroup">Show
+	<select id=selectedGroup>
+		<option>All students</option>
+		<option value="690511">Group 1</option>
+		<option value="690513">Group 2</option>
+	</select>
+	<h3>Selected Students</h3>
+	<table>
+		<thead>
+			<tr>
+				<th>First Name</th>
+				<th>Last Name</th>
+				<th>SRN</th>
+				<th>Allocated Group</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>Jo 1</td>
+				<td>Bloggs</td>
+				<td>97xx08xx01</td>
+				<td>Group 1</td>
+			</tr>
+			<tr>
+				<td>Joe 3</td>
+				<td>Bliggs</td>
+				<td>97xx08xx04</td>
+				<td>Group 2</td>
+			</tr>
+		</tbody>
+
+		<?php
+			// Create connection
+			$conn = new mysqli("localhost", "root", "", "goParks");
+
+			// Execute query
+			$q = 'select displayName, council, atmosphere from parks';
+			$result = mysqli_query($conn,$q);
+
+			// Output results in table rows
+			if ($result->num_rows > 0) {
+				// output data of each row
+				while($row = $result->fetch_assoc()) {
+					echo 	"<tr>
+								<td>".$row["displayName"]."</td>
+								<td>".$row["council"]."</td>
+								<td>".$row["atmosphere"]."</td>
+							</tr>";
+				}
+			} else {
+				echo 	"<tr>
+							<td>No parks found</td>
+						</tr>";
+			}
+		?>
+	</table>
+</body>
+</html>
